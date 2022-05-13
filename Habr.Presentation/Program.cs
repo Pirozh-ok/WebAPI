de22a33f771e05d23a45fb4c8718c4cb014d399d
Ivan Vorotnikov <ivan.vorotnikov.2002@mail.ru>
2022-05-13 12:24:36 +0700

    Moved service registration to extension methods

--- Habr.Presentation/Program.cs
+++ Habr.Presentation/Program.cs
@@ -1,22 +1,13 @@
-﻿using Habr.BusinessLogic.Services.Implementations;
-using Habr.BusinessLogic.Services.Interfaces;
+﻿using Habr.BusinessLogic;
 using Habr.Common.AutoMappers;
 using Habr.DataAccess;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Getting the connection string from the configuration file.
-string connection = builder.Configuration.GetConnectionString("DefaultConnection");
-
 // Add services to the container.
 builder.Services.AddControllers();
-
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IPostService, PostService>();
-builder.Services.AddScoped<ICommentService, CommentService>();
-
-builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connection));
+builder.Services.AddServices();
+builder.Services.ConfigureServices(builder.Configuration);
 builder.Services.AddAutoMapper(typeof(AppMappingProfile));
 
 var app = builder.Build();
