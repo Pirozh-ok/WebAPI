8d4347cfaa3c959fbf2ae62a64e6afd1bdb1e466
Ivan Vorotnikov <ivan.vorotnikov.2002@mail.ru>
2022-05-20 00:20:33 +0700

    Add swagger

--- Habr.Presentation/Program.cs
+++ Habr.Presentation/Program.cs
@@ -14,11 +14,20 @@ builder.Services.AddControllersWithViews()
 builder.Services.AddServices();
 builder.Services.ConfigureServices(builder.Configuration);
 builder.Services.AddAutoMapper(typeof(PostProfile), typeof(CommentProfile), typeof(UserProfile));
+builder.Services.AddSwaggerDocument(config =>
+{
+    config.PostProcess = document =>
+    {
+        document.Info.Version = "v1";
+        document.Info.Title = "Habr API";
+    };
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseOpenApi();
+app.UseSwaggerUi3();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
